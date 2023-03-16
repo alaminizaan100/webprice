e4580289f5c778274848a860afc8992e8f8cb01e
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-16 21:53:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import ccxtpro
 
 app = Flask(__name__)
@@ -20,17 +20,21 @@ def index():
         if ticker['symbol'] == 'BTC/USDT':
             binance_price = ticker['last']
             print(f'Binance BTC/USDT price: {binance_price}')
-            return jsonify({'exchange': 'Binance', 'pair': 'BTC/USDT', 'price': binance_price})
+            return binance_price
 
     @kucoin.on('ticker')
     async def kucoin_ticker_update(ticker, client):
         if ticker['symbol'] == 'BTC/USDT':
             kucoin_price = ticker['last']
             print(f'KuCoin BTC/USDT price: {kucoin_price}')
-            return jsonify({'exchange': 'KuCoin', 'pair': 'BTC/USDT', 'price': kucoin_price})
+            return kucoin_price
 
     binance.start()
     kucoin.start()
+
+    binance_price = binance.fetch_ticker('BTC/USDT')['last']
+    kucoin_price = kucoin.fetch_ticker('BTC/USDT')['last']
+    return render_template('index.html', binance_price=binance_price, kucoin_price=kucoin_price)
 
 if __name__ == '__main__':
     app.run()

b2267a3d68f26e40a8893749fe9d64913d048232
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-19 18:15:43 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,79 @@
-from flask import Flask, jsonify, render_template
-import websocket
-import json
-import threading
+
+from binance import AsyncClient, BinanceSocketManager
+import asyncio
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/binance')
-def binance():
-    data_dict = {}
+# Define your Binance API keys
+API_KEY = 'OtmdN18Tgx7VjnLyD4Ulc7ooNUaS0ezw38EZtTXvz0Eln4LxePIGCjOC95WG80OG'
+API_SECRET = 'ShmYzH63927bieEp6SgHTDXv3hlEdkiePHMsSpdXpbviKNJbGpPSS6M3YSTACq4u'
 
-    def on_message(ws, message):
-        data = json.loads(message)
-        if data['e'] == 'error':
-            print(f"Error: {data['m']}")
-            return
-        if data['e'] == '24hrTicker':
-            symbol = data['s']
-            price = data['c']
-            # Add the data to a dictionary
-            data_dict[symbol] = price
+# Create a Binance client and socket manager
+client = AsyncClient(API_KEY, API_SECRET)
+bm = BinanceSocketManager(client)
 
-    def on_error(ws, error):
-        print(f"Error: {error}")
+# Define the websocket endpoint to connect to
+ws_endpoint = '!ticker@arr'
 
-    def on_close(ws):
-        print("Connection closed")
+# Define the callback function for handling the websocket data
+async def handle_socket_message(msg):
+    # Send the websocket message to the index.html file using Flask-SocketIO
+    await app.extensions['socketio'].emit('ticker', msg)
 
-    def on_open(ws):
-        print("Connection opened")
-        ws.send('{"method": "SUBSCRIBE","params": ["!ticker@arr"],"id": 1}')
+# Define the route to render the HTML template
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    def run_ws():
-        websocket.enableTrace(True)
-        ws = websocket.WebSocketApp("wss://stream.binance.com:9443/ws/!ticker@arr", on_message=on_message, on_error=on_error, on_close=on_close)
-        ws.on_open = on_open
-        ws.run_forever()
+# Define the function to start the websocket connection
+async def start_websocket():
+    # Create a websocket connection to the endpoint
+    ws = bm.multiplex_socket([ws_endpoint])
 
-    # Start the WebSocket connection in a new thread
-    ws_thread = threading.Thread(target=run_ws)
-    ws_thread.start()
+    # Register the callback function to handle incoming data
+    ws.register_callback(handle_socket_message)
 
-    # Return the data as a JSON response
-    return render_template('index.html', data_dict=data_dict)
+    # Start the websocket connection
+    await ws.__aenter__()
+
+# Start the websocket connection when the Flask app starts
+@app.before_first_request
+async def start():
+    await start_websocket()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Start the Flask app using Gunicorn
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    from gunicorn.workers.gthread import GThreadWorker
+    from socketio import Server
+
+    socketio = Server(async_mode='threading')
+    app.extensions['socketio'] = socketio
+
+    @app.before_first_request
+    def start_background_thread():
+        async def run():
+            await start_websocket()
+        asyncio.create_task(run())
+
+    @socketio.on('connect')
+    def connect():
+        print('Client connected')
+
+    @socketio.on('disconnect')
+    def disconnect():
+        print('Client disconnected')
+
+    @socketio.on_error()
+    def error_handler(e):
+        print('Error:', e)
+
+    @socketio.on('ticker')
+    def handle_ticker(msg):
+        socketio.emit('ticker', msg)
+
+    app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+    worker = GThreadWorker(app)
+    worker.init_app(app)
+    worker.run()

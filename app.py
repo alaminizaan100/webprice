fea4a62756ec63c8f315f89a919c466f59f443b1
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-19 16:42:51 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify
+import websocket
+import json
+
+app = Flask(__name__)
+
+prices = []
+
+def on_message(ws, message):
+    data = json.loads(message)
+    if data['e'] == 'error':
+        print(f"Error: {data['m']}")
+        return
+    if data['e'] == '24hrTicker':
+        symbol = data['s']
+        price = data['c']
+        prices.append({symbol: price})
+
+def on_error(ws, error):
+    print(f"Error: {error}")
+
+def on_close(ws):
+    print("Connection closed")
+
+def on_open(ws):
+    print("Connection opened")
+    ws.send('{"method": "SUBSCRIBE","params": ["!ticker@arr"],"id": 1}')
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/binance')
+def binance():
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://stream.binance.com:9443/ws/!ticker@arr", on_message = on_message, on_error = on_error, on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+    return jsonify(prices)
+
+if __name__ == '__main__':
+    app.run()

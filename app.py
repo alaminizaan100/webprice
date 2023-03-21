5adfe74b57f2810eb332297ac742326f9e21b69d
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-21 17:17:46 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 from flask import Flask
+
 import ccxt.async_support as ccxt
 import asyncio
 
 app = Flask(__name__)
 
-# Set up the Binance WebSocket connection
 async def binance_ws(symbol):
     exchange = ccxt.binance()
     ticker = await exchange.subscribe_ticker(symbol)
     while True:
-        data = await ticker.recv()
-        # Convert the price to USD
-        price = await exchange.fetch_ticker(symbol)
-        usd_price = price['last'] * await exchange.fetch_ticker('BTC/USDT')['last']
-        # Print the new ticker data in USD
-        print(f'{symbol}: {usd_price}')
+        try:
+            data = await ticker.recv()
+            # Convert the price to USD
+            price = await exchange.fetch_ticker(symbol)
+            usd_price = price['last'] * await exchange.fetch_ticker('BTC/USDT')['last']
+            # Print the new ticker data in USD
+            print(f'{symbol}: {usd_price}')
+        except Exception as e:
+            print(f'Error in WebSocket connection: {e}')
 
-# Set up the Flask route and start the WebSocket connection
 @app.route('/')
 def index():
     symbol = 'BTC/USDT'
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    binance_ws_task = loop.create_task(binance_ws(symbol))
-    return 'Binance WebSocket connection started'
+    binance_ws_task = asyncio.create_task(binance_ws(symbol))
+    return "Binance WebSocket connection started"
 
 if __name__ == '__main__':
     app.run()

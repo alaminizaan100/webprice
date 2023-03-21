52b45b55350151e10cb691156b6f89cb32242435
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-21 11:40:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,36 @@
 import ccxt.async_support as ccxt
 import asyncio
+from aiohttp import ClientSession
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['TIMEOUT'] = 180  # Set server timeout to 3 minutes
-async def scan_arbi(exchanges):
-    result = []
+app.config['TIMEOUT'] = 180
+
+async def fetch_ticker(exchange, symbol, session):
+    ticker = await exchange.fetch_ticker(symbol)
+    return (exchange.id, symbol, ticker['last'])
+
+async def get_tickers(exchange, symbols, session):
+    tasks = []
+    for symbol in symbols:
+        tasks.append(fetch_ticker(exchange, symbol, session))
+    results = await asyncio.gather(*tasks)
+    return results
+
+async def scan_arbi(exchanges, session):
+    symbols = ['BTC/USDT', 'ETH/USDT', 'LTC/USDT']
+    tasks = []
     for exchange in exchanges:
-        await exchange.load_markets()
-
-    # Check for arbitrage opportunities
-    for i in range(len(exchanges)):
-        for j in range(i+1, len(exchanges)):
-            exchange1 = exchanges[i]
-            exchange2 = exchanges[j]
-
-            # Get all symbols with both exchanges
-            symbols = set(exchange1.symbols) & set(exchange2.symbols)
-
-            # Check for arbitrage opportunities in each symbol
-            for symbol in symbols:
-                try:
-                    ticker1 = await exchange1.fetch_ticker(symbol)
-                    ticker2 = await exchange2.fetch_ticker(symbol)
-
-                    # Cal profit percentage
-                    bid1 = ticker1['bid']
-                    ask2 = ticker2['ask']
-                    profit_percent = (ask2 - bid1) / bid1 * 100
-
-                    #  percentage is positive
-                    if profit_percent > 0:
-                        result.append({
-                            'symbol': symbol,
-                            'exchange1': exchange1.id,
-                            'exchange2': exchange2.id,
-                            'bid1': bid1,
-                            'ask2': ask2,
-                            'profit_percent': profit_percent
-                        })
-                except ccxt.errors.ExchangeError as e:
-                    print(f"Error fetching ticker for {symbol} on {exchange1.id} and {exchange2.id}: {e}")
-
-    return result
+        tasks.append(get_tickers(exchange, symbols, session))
+    results = await asyncio.gather(*tasks)
+    return results
 
 @app.route('/')
-def index():
-    # Create a list of exchange instances
-    exchanges = [ccxt.binance(), ccxt.bitfinex()]
-
-    # Scan for arbitrage opportunities
-    loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(scan_arbi(exchanges))
-
-    return render_template('index.html', result=result)
+async def index():
+    async with ClientSession() as session:
+        exchanges = [ccxt.binance(), ccxt.bitfinex()]
+        results = await scan_arbi(exchanges, session)
+    return render_template('index.html', result=results)
 
 if __name__ == '__main__':
     app.run(debug=True)

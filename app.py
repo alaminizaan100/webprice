cdaa53f4b2936443a3c44f1eac6cde5af7810722
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-22 09:23:49 +0600

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def binance_data():
                     rate_1 = coins[base_asset][quote_asset_1] * 0.999 # Apply 0.1% trading fee
                     rate_2 = coins[quote_asset_1][quote_asset_2] * 0.999 # Apply 0.1% trading fee
                     rate_3 = coins[quote_asset_2][base_asset] * 0.999 # Apply 0.1% trading fee
-                    potential_profit = (100 / rate_1) * rate_2 * rate_3 - 100 # Calculate potential profit with initial investment of 100 USD
+                    potential_profit = ((100 / rate_1) * rate_2 * rate_3 - 100) * 100 / 100 # Calculate potential profit with initial investment of 100 USD and convert to percentage
                     if potential_profit > 0:
                         opportunity = {
                             'base_asset': base_asset,
@@ -64,7 +64,18 @@ def binance_data():
     opportunities = sorted(opportunities, key=lambda x: x['potential_profit'], reverse=True)
     num_opportunities = len(opportunities)
 
+    for opportunity in opportunities:
+        base_asset = opportunity['base_asset']
+        quote_asset_1 = opportunity['quote_asset_1']
+        quote_asset_2 = opportunity['quote_asset_2']
+        profit = opportunity['potential_profit']
+        buy_sell = base_asset + "/" + quote_asset_1 + "/" + quote_asset_2
+        sell_buy = quote_asset_2 + "/" + quote_asset_1 + "/" + base_asset
+        opportunity['buy_sell'] = buy_sell
+        opportunity['sell_buy'] = sell_buy
+
     return render_template('index.html', opportunities=opportunities, num_opportunities=num_opportunities)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

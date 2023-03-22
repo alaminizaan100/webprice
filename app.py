54a834f24114cb2bf86fb82d1b003c259c8c6b9c
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-23 00:55:49 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-from arbitrage import find_arbitrage_opportunities
+from arbitrage import find_arbitrage_opportunities, get_ticker_data, get_exchange_info
 
 app = Flask(__name__)
 
 @app.route('/')
 def binance_data():
-    opportunities, num_opportunities = find_arbitrage_opportunities()
+    ticker_data = get_ticker_data()
+    exchange_info = get_exchange_info()
+    opportunities = calculate_arbitrage_opportunities(ticker_data, exchange_info)
+    num_opportunities = len(opportunities)
 
     return render_template('index.html', opportunities=opportunities, num_opportunities=num_opportunities)
 

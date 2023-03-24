fc72b6178a9c8afda7935e3ac4c316c0963ad144
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-24 18:28:20 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,39 @@
 from flask import Flask, render_template
-from binance.client import Client
+import requests
 
 app = Flask(__name__)
-client = Client()
 
 @app.route('/')
 def index():
-    pairs = get_active_pairs()
+    pairs = get_trading_pairs()
     triangles = find_triangular_arbitrage(pairs)
     return render_template('index.html', pairs=pairs, triangles=triangles)
 
-def get_active_pairs():
+def get_trading_pairs():
+    response = requests.get('https://api.binance.com/api/v3/exchangeInfo')
     pairs = []
-    tickers = client.get_all_tickers()
-    for ticker in tickers:
-        symbol = ticker['symbol']
-        if symbol.endswith('BTC') or symbol.endswith('USDT'):
-            pairs.append(symbol)
+    for symbol in response.json()['symbols']:
+        if symbol['status'] == 'TRADING' and (symbol['baseAsset'] == 'BTC' or symbol['baseAsset'] == 'USDT'):
+            pairs.append(symbol['symbol'])
     return pairs
 
 def find_triangular_arbitrage(pairs):
     triangles = []
-    for pair1 in pairs:
-        for pair2 in pairs:
-            if pair1 == pair2:
-                continue
-            for pair3 in pairs:
-                if pair3 == pair1 or pair3 == pair2:
-                    continue
-                if check_arbitrage_opportunity(pair1, pair2, pair3):
-                    triangles.append((pair1, pair2, pair3))
+    for i in range(len(pairs)):
+        for j in range(i + 1, len(pairs)):
+            for k in range(j + 1, len(pairs)):
+                pair1, pair2, pair3 = pairs[i], pairs[j], pairs[k]
+                price1 = get_price(pair1)
+                price2 = get_price(pair2)
+                price3 = get_price(pair3)
+                implied_rate = price1 * price2 / price3
+                if implied_rate > 1.01:
+                    triangles.append((pair1, pair2, pair3, implied_rate))
     return triangles
 
-def check_arbitrage_opportunity(pair1, pair2, pair3):
-    # TODO: Implement triangular arbitrage check using Binance API
-    # For example, use client.get_order_book(symbol=pair1) to retrieve the order book
-    return False
+def get_price(symbol):
+    response = requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={symbol}')
+    return float(response.json()['price'])
 
 if __name__ == '__main__':
     app.run(debug=True)

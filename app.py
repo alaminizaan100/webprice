a9780472f186dd4938fcbf3875e09ba1717100b5
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-25 10:46:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,69 +3,44 @@ import requests
 
 app = Flask(__name__)
 
-EXCHANGES = [
-    {
-        "name": "binance",
-        "flags": ["triangular_arbitrage"]
-    },
-    {
-        "name": "kucoin",
-        "flags": ["triangular_arbitrage"]
-    }
-]
+# Binance API endpoints
+BASE_URL = "https://api.binance.com/api/v3"
+EXCHANGE_INFO_URL = BASE_URL + "/exchangeInfo"
+TICKER_PRICE_URL = BASE_URL + "/ticker/price"
 
-# Define function to get coin information from API
-def get_coin_info():
-    url = "https://api.coingecko.com/api/v3/coins/list"
-    while url:
-        response = requests.get(url)
-        if not response.ok:
-            raise Exception("Failed to get coin list from Coingecko API.")
-        coin_list = response.json()
-        for coin in coin_list:
-            yield coin
-        url = response.links.get("next", {}).get("url")
-
-# Define function to check for triangular arbitrage opportunities
-def check_triangular_arbitrage(exchange_list):
-    arbitrage_exchange = []
-    for exchange in exchange_list:
-        if "triangular_arbitrage" in exchange.get("flags", []):
-            arbitrage_exchange.append(exchange["name"])
-    return arbitrage_exchange
-
-# Define route to display all coin information and check for triangular arbitrage opportunities
 @app.route("/")
-def display_coin_info():
-    arbitrage_exchange = check_triangular_arbitrage(EXCHANGES)
-    coin_info_list = []
-    for coin in get_coin_info():
-        coin_info = {
-            "id": coin["id"],
-            "symbol": coin["symbol"],
-            "name": coin["name"]
-        }
-        for exchange in arbitrage_exchange:
-            try:
-                arbitrage_profit = get_triangular_arbitrage_profit(coin["id"], exchange)
-                if arbitrage_profit is not None:
-                    coin_info[f"{exchange}_arbitrage_profit"] = arbitrage_profit
-            except Exception as e:
-                print(f"Error getting triangular arbitrage profit for {coin['id']} on {exchange}: {str(e)}")
-        coin_info_list.append(coin_info)
-    return render_template("index.html", coin_info_list=coin_info_list)
+def home():
+    # Get exchange info from Binance API
+    exchange_info = requests.get(EXCHANGE_INFO_URL).json()
 
-# Define function to get triangular arbitrage profit for a given coin and exchange
-def get_triangular_arbitrage_profit(coin_id, exchange_name):
-    url = f"https://api.coingecko.com/api/v3/exchanges/{exchange_name}/triangular_arbitrage?currency_pairs={coin_id}"
-    response = requests.get(url)
-    if not response.ok:
-        raise Exception(f"Failed to get triangular arbitrage data from Coingecko API for {coin_id} on {exchange_name}.")
-    data = response.json()
-    if not data or "error" in data:
-        raise Exception(f"No triangular arbitrage data available for {coin_id} on {exchange_name}.")
-    profit = float(data[0]["rate"]) - 1
-    return profit
+    # Get ticker prices from Binance API
+    ticker_prices = requests.get(TICKER_PRICE_URL).json()
 
-if __name__ == '__main__':
+    # Filter out symbols that are not available for trading
+    symbols_available_for_trading = [symbol for symbol in exchange_info["symbols"] if symbol["status"] == "TRADING"]
+
+    # Calculate triangular arbitrage profit for each symbol
+    triangular_arbitrage_profits = []
+    for symbol in symbols_available_for_trading:
+        # Extract base and quote currencies
+        base_currency = symbol["baseAsset"]
+        quote_currency = symbol["quoteAsset"]
+        
+        # Check if there are any pairs that can be used for triangular arbitrage
+        for pair in exchange_info["symbols"]:
+            if pair["baseAsset"] == quote_currency:
+                for pair2 in exchange_info["symbols"]:
+                    if pair2["baseAsset"] == pair["quoteAsset"] and pair2["quoteAsset"] == base_currency:
+                        # Calculate triangular arbitrage profit assuming no trading fee and initial investment of 100 USDT
+                        rate1 = float([price["price"] for price in ticker_prices if price["symbol"] == symbol["symbol"]][0])
+                        rate2 = float([price["price"] for price in ticker_prices if price["symbol"] == pair["symbol"]][0])
+                        rate3 = float([price["price"] for price in ticker_prices if price["symbol"] == pair2["symbol"]][0])
+                        triangular_arbitrage_profit = 100 * rate1 / rate2 * rate3 / 100 - 100
+                        if triangular_arbitrage_profit > 0:
+                            triangular_arbitrage_profits.append((symbol["symbol"], triangular_arbitrage_profit))
+
+    # Render template with data
+    return render_template("index.html", symbols=symbols_available_for_trading, triangular_arbitrage_profits=triangular_arbitrage_profits)
+
+if __name__ == "__main__":
     app.run(debug=True)

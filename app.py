6d5fb775fad3558b79535abefc0d1dea79bfe710
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-25 23:05:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,114 +1,44 @@
-import requests
-from flask import Flask, jsonify
+from flask import Flask, render_template
+import ccxt
 
 app = Flask(__name__)
 
-# Constants
-BINANCE_API_URL = "https://api.binance.com/api/v3"
-KUCOIN_API_URL = "https://api.kucoin.com/api/v1"
-MIN_VOLUME = 100  # Minimum trading volume in USDT for a coin to be considered for arbitrage
-
-# Helper functions to get trading pairs for each exchange
-def get_binance_pairs(coin):
-    url = f"{BINANCE_API_URL}/exchangeInfo"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        available_pairs = []
-        for symbol in data["symbols"]:
-            if symbol["quoteAsset"] == "USDT" and symbol["status"] == "TRADING":
-                if symbol["baseAsset"] == coin:
-                    available_pairs.append(symbol["symbol"])
-        return available_pairs
-    return []
-
-def get_kucoin_pairs(coin):
-    url = f"{KUCOIN_API_URL}/market/open/symbols"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        available_pairs = []
-        for symbol in data["data"]:
-            if symbol["coinType"] == coin and symbol["quoteCurrency"] == "USDT" and symbol["trading"]:
-                volume_url = f"{KUCOIN_API_URL}/market/stats?symbol={symbol['symbol']}"
-                volume_response = requests.get(volume_url)
-                if volume_response.status_code == 200:
-                    volume_data = volume_response.json()
-                    if volume_data['data']['volValue'] >= MIN_VOLUME:
-                        available_pairs.append(symbol["symbol"])
-        return available_pairs
-    return []
-
-# Main endpoint to get arbitrage opportunities between Binance and Kucoin
-@app.route("/arbitrage/<coin>")
-def get_arbitrage_opportunities(coin):
-    # Get available trading pairs for the specified coin on each exchange
-    binance_pairs = get_binance_pairs(coin)
-    kucoin_pairs = get_kucoin_pairs(coin)
-
-    # Check for arbitrage opportunities between the two exchanges
+@app.route('/')
+def index():
+    # Exchange objects for Binance and KuCoin
+    binance = ccxt.binance()
+    kucoin = ccxt.kucoin()
+    
+    # Get all the markets available on both exchanges
+    binance_markets = binance.load_markets()
+    kucoin_markets = kucoin.load_markets()
+    
+    # Find the common markets between Binance and KuCoin
+    common_markets = set(binance_markets.keys()) & set(kucoin_markets.keys())
+    
+    # Create a list of dictionaries for all the common markets with arbitrage opportunity
     arbitrage_opportunities = []
-    for pair in binance_pairs:
-        if pair in kucoin_pairs:
-            # Get trading fees for the pair on each exchange
-            binance_fee_url = f"{BINANCE_API_URL}/ticker/24hr?symbol={pair}"
-            binance_fee_response = requests.get(binance_fee_url)
-            if binance_fee_response.status_code == 200:
-                binance_fee_data = binance_fee_response.json()
-                binance_fee = float(binance_fee_data["quoteVolume"]) * float(binance_fee_data["bidPrice"]) * 0.001
-            else:
-                binance_fee = 0
+    for market in common_markets:
+        # Get the ticker information for the market on both exchanges
+        binance_ticker = binance.fetch_ticker(market)
+        kucoin_ticker = kucoin.fetch_ticker(market)
+        
+        # Calculate the arbitrage opportunity
+        amount_bought = (100 / binance_ticker['ask']) * (1 - binance.fees['trading']['taker'])
+        amount_sold = amount_bought * kucoin_ticker['bid'] * (1 - kucoin.fees['trading']['taker']) - kucoin.fees['withdraw'][kucoin_ticker['symbol'].split('/')[0]]
+        arbitrage = amount_sold - 100
+        
+        # If the arbitrage opportunity is positive, add it to the list of opportunities
+        if arbitrage > 0:
+            withdraw_fee = binance.fees['withdraw'][binance_ticker['symbol'].split('/')[0]]
+            arbitrage_opportunities.append({
+                'market': market,
+                'arbitrage': round(arbitrage, 2),
+                'withdraw_fee': withdraw_fee
+            })
+    
+    # Render the HTML template with the list of opportunities
+    return render_template('index.html', opportunities=arbitrage_opportunities)
 
-            kucoin_fee_url = f"{KUCOIN_API_URL}/market/orderbook/level1?symbol={pair}"
-            kucoin_fee_response = requests.get(kucoin_fee_url)
-            if kucoin_fee_response.status_code == 200:
-                kucoin_fee_data = kucoin_fee_response.json()
-                kucoin_fee = float(kucoin_fee_data["data"]["bestBid"]) * 0.001
-            else:
-                kucoin_fee = 0
-
-            # Get coin transfer fees for the specified coin on each exchange
-            binance_transfer_fee_url = f"{BINANCE_API_URL}/asset/assetDetail"
-            binance_transfer_fee_response = requests.get(binance_transfer_fee_url)
-            if binance_transfer_fee_response.status_code == 200:
-                binance_transfer_fee_data = binance_transfer_fee_response.json()
-                binance_transfer_fee = float(binance_transfer_fee_data[coin]["withdrawFee"])
-            else:
-                binance_transfer_fee = 0
-
-            kucoin_transfer_fee_url = f"{KUCOIN_API_URL}/wallet/withdraw/fee?currency={coin}"
-            kucoin_transfer_fee_response = requests.get(kucoin_transfer_fee_url)
-            if kucoin_transfer_fee_response.status_code == 200:
-                kucoin_transfer_fee_data = kucoin_transfer_fee_response.json()
-                kucoin_transfer_fee = float(kucoin_transfer_fee_data["withdrawMinFee"])
-            else:
-                kucoin_transfer_fee = 0
-
-            # Calculate potential profit margin for the arbitrage opportunity
-            binance_price_url = f"{BINANCE_API_URL}/ticker/price?symbol={pair}"
-            binance_price_response = requests.get(binance_price_url)
-            kucoin_price_url = f"{KUCOIN_API_URL}/market/orderbook/level1?symbol={pair}"
-            kucoin_price_response = requests.get(kucoin_price_url)
-
-            if binance_price_response.status_code == 200 and kucoin_price_response.status_code == 200:
-                binance_price_data = binance_price_response.json()
-                kucoin_price_data = kucoin_price_response.json()
-                binance_price = float(binance_price_data["price"])
-                kucoin_price = float(kucoin_price_data["data"]["bestBid"])
-                potential_profit_margin = (kucoin_price - binance_price) / binance_price
-                # Append the arbitrage opportunity to the list
-                arbitrage_opportunities.append({
-                    "pair": pair,
-                    "binance_fee": binance_fee,
-                    "kucoin_fee": kucoin_fee,
-                    "binance_transfer_fee": binance_transfer_fee,
-                    "kucoin_transfer_fee": kucoin_transfer_fee,
-                    "potential_profit_margin": potential_profit_margin
-                })
-
-    # Sort the arbitrage opportunities by potential profit margin (highest to lowest)
-    sorted_arbitrage_opportunities = sorted(arbitrage_opportunities, key=lambda x: x["potential_profit_margin"], reverse=True)
-
-    # Return the sorted list of arbitrage opportunities as JSON
-    return jsonify(sorted_arbitrage_opportunities)
-
+if __name__ == '__main__':
+    app.run(debug=True)

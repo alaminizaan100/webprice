f52542a72ae2ab84606ad59dc46a2e0863afdf1f
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-03-27 20:10:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,85 @@
+# Import necessary libraries
 from flask import Flask, render_template
 import requests
-import json
 
+# Create Flask application
 app = Flask(__name__)
 
-def get_exchange_data():
-    exchanges = {
-        "binance": "https://api.binance.com/api/v3/ticker/price",
-        "bitfinex": "https://api.bitfinex.com/v1/pubticker/BTCUSD",
-        "bittrex": "https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC",
-        "coinbase": "https://api.coinbase.com/v2/prices/BTC-USD/spot",
-        "kraken": "https://api.kraken.com/0/public/Ticker?pair=XBTUSD"
-    }
+# Define a route to display the prices
+@app.route('/prices')
+def display_prices():
+    # Define a dictionary to store the prices from all 10 exchanges
+    prices = {}
 
-    exchange_data = {}
-    for exchange, url in exchanges.items():
-        response = requests.get(url)
-        data = json.loads(response.text)
+    # Define a list of tuples with the exchange name and its API endpoint
+    exchanges = [
+        ('Binance', 'https://api.binance.com/api/v3/ticker/price'),
+        ('Bitfinex', 'https://api.bitfinex.com/v1/pubticker/all'),
+        ('Bitstamp', 'https://www.bitstamp.net/api/v2/ticker_hour/'),
+        ('Coinbase Pro', 'https://api.pro.coinbase.com/products/ticker'),
+        ('Kraken', 'https://api.kraken.com/0/public/Ticker'),
+        ('Huobi', 'https://api.huobi.pro/market/tickers'),
+        ('OKEx', 'https://www.okex.com/api/spot/v3/instruments/ticker'),
+        ('Upbit', 'https://api.upbit.com/v1/ticker'),
+        ('Bithumb', 'https://api.bithumb.com/public/ticker/all'),
+        ('Bitflyer', 'https://api.bitflyer.com/v1/getticker'),
+    ]
 
-        # extract data for all coins
-        if exchange == "binance":
+    # Loop through each exchange and extract the prices for all available trading coins
+    for exchange, endpoint in exchanges:
+        response = requests.get(endpoint)
+        data = response.json()
+
+        # Extract the prices for all available trading coins
+        if exchange == 'Binance':
             for coin in data:
-                if coin["symbol"].endswith("USDT"):
-                    symbol = coin["symbol"][:-4]
-                    price = float(coin["price"])
-                    if symbol in exchange_data:
-                        exchange_data[symbol][exchange] = price
-                    else:
-                        exchange_data[symbol] = {exchange: price}
-        elif exchange == "bitfinex":
-            symbol = "BTC"
-            price = float(data["last_price"])
-            if symbol in exchange_data:
-                exchange_data[symbol][exchange] = price
-            else:
-                exchange_data[symbol] = {exchange: price}
-        elif exchange == "bittrex":
-            symbol = "BTC"
-            price = float(data["result"]["Last"])
-            if symbol in exchange_data:
-                exchange_data[symbol][exchange] = price
-            else:
-                exchange_data[symbol] = {exchange: price}
-        elif exchange == "coinbase":
-            symbol = "BTC"
-            price = float(data["data"]["amount"])
-            if symbol in exchange_data:
-                exchange_data[symbol][exchange] = price
-            else:
-                exchange_data[symbol] = {exchange: price}
-        elif exchange == "kraken":
-            symbol = "BTC"
-            price = float(data["result"]["XXBTZUSD"]["c"][0])
-            if symbol in exchange_data:
-                exchange_data[symbol][exchange] = price
-            else:
-                exchange_data[symbol] = {exchange: price}
+                symbol = coin['symbol']
+                price = float(coin['price'])
+                prices[f"{exchange} - {symbol}"] = price
+        elif exchange == 'Bitfinex':
+            for coin, info in data.items():
+                symbol = coin.upper()
+                price = float(info['last_price'])
+                prices[f"{exchange} - {symbol}"] = price
+        elif exchange == 'Bitstamp':
+            for coin, info in data.items():
+                symbol = coin.upper()
+                price = float(info['last'])
+                prices[f"{exchange} - {symbol}"] = price
+        elif exchange == 'Coinbase Pro':
+            for coin in data:
+                symbol = coin['product_id'].replace('-', '/')
+                price = float(coin['price'])
+                prices[f"{exchange} - {symbol}"] = price
+        elif exchange == 'Kraken':
+            for symbol, info in data['result'].items():
+                price = float(info['c'][0])
+                prices[f"{exchange} - {symbol}"] = price
+        elif exchange == 'Huobi':
+            for coin in data['data']:
+                symbol = coin['symbol'].replace('_', '/')
+                price = float(coin['close'])
+                prices[f"{exchange} - {symbol}"] = price
+        elif exchange == 'OKEx':
+            for coin in data:
+                symbol = coin['instrument_id'].replace('-', '/')
+                price = float(coin['last'])
+                prices[f"{exchange} - {symbol}"] = price
+        elif exchange == 'Upbit':
+            for coin in data:
+                symbol = coin['market'].replace('-', '/')
+                price = float(coin['trade_price'])
+                prices[f"{exchange} - {symbol}
+                elif exchange == 'Bithumb':
+            for coin, info in data['data'].items():
+                symbol = coin.replace('_', '/')
+                price = float(info['closing_price'])
+                prices[f"{exchange} - {symbol}"] = price
+        elif exchange == 'Bitflyer':
+            for coin in data:
+                symbol = coin['product_code'].replace('_', '/')
+                price = float(coin['ltp'])
+                prices[f"{exchange} - {symbol}"] = price
 
-    return exchange_data
-
-def get_arbitrage_opportunities(exchange_data):
-    opportunities = []
-    for coin, prices in exchange_data.items():
-        max_price = max(prices.values())
-        min_price = min(prices.values())
-        if max_price > min_price:
-            arb = ((max_price - min_price) / min_price) * 100
-            opportunities.append({"coin": coin, "arb": arb})
-    return sorted(opportunities, key=lambda x: x["arb"], reverse=True)
-
-@app.route("/")
-def home():
-    exchange_data = get_exchange_data()
-    opportunities = get_arbitrage_opportunities(exchange_data)
-    return render_template("home.html", exchange_data=exchange_data, opportunities=opportunities)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    # Render the HTML template with the prices
+    return render_template('prices.html', prices=prices)

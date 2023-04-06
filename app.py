a3eccadf4e3c036261a2d3b6748dafe829ddc578
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-04-06 19:27:03 +0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
     exchanges = {
         'binance': ccxt.binance(),
         'kucoin': ccxt.kucoin(),
-        #'bittrex': ccxt.bittrex(),
+        'bittrex': ccxt.bittrex(),
         'poloniex': ccxt.poloniex(),
       #  'kraken': ccxt.kraken(),
      #   'bitfinex': ccxt.bitfinex(),

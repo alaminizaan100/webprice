291f4f67021319ec8173704630863d97655883d8
alaminizaan100 <128066487+alaminizaan100@users.noreply.github.com>
2023-04-06 14:53:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,56 @@
 from flask import Flask, render_template
+
 import requests
+
 app = Flask(__name__)
+
 @app.route('/')
+
 def index():
-    # Get current trading prices for Bitcoin, Ethereum, and Litecoin from CoinGecko API
-    url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,litecoin&vs_currencies=usd&include_24hr_change=true'
-    data = requests.get(url).json()
-    # Get top 10 crypto exchanges from CoinGecko API
-    url = 'https://api.coingecko.com/api/v3/exchanges?per_page=10'
-    exchanges = requests.get(url).json()
-    # Create a dictionary to store prices and arbitrage for each coin
-    prices = {}
-    arbitrage = {}
-    # Loop through the top 10 exchanges and get trading prices for each coin
-    for exchange in exchanges:
-        exchange_name = exchange['name']
-        exchange_id = exchange['id']
-        exchange_url = f'https://api.coingecko.com/api/v3/exchanges/{exchange_id}?tickers=true&per_page=100&page=1'
-        exchange_data = requests.get(exchange_url).json()
-        tickers = exchange_data['tickers']
-        for ticker in tickers:
-            if ticker['target'] == 'USD':
-                coin = ticker['base']
-                price = ticker['last']
-                change = ticker['converted_last']['usd_24h_change']
-                if coin not in prices:
-                    prices[coin] = {}
-                    arbitrage[coin] = {}
-                prices[coin][exchange_name] = price
-                arbitrage[coin][exchange_name] = None
-    # Calculate arbitrage for each coin
-    for coin in prices:
-        for exchange1 in prices[coin]:
-            for exchange2 in prices[coin]:
-                if exchange1 != exchange2:
-                    arb = prices[coin][exchange2] - prices[coin][exchange1]
-                    if arbitrage[coin][exchange1] is None or arb > arbitrage[coin][exchange1]:
-                        arbitrage[coin][exchange1] = arb
-    # Render template with prices and arbitrage for each coin
-    return render_template('index.html', prices=prices, arbitrage=arbitrage)
+
+    binance_url = "https://api.binance.com/api/v3/ticker/price"
+
+    kucoin_url = "https://api.kucoin.com/api/v1/market/allTickers"
+
+    binance_response = requests.get(binance_url)
+
+    kucoin_response = requests.get(kucoin_url)
+
+    binance_data = binance_response.json()
+
+    kucoin_data = kucoin_response.json()['data']['ticker']
+
+    binance_prices = {item['symbol']: float(item['price']) for item in binance_data}
+
+    kucoin_prices = {item['symbol']: float(item['last']) for item in kucoin_data}
+
+    common_symbols = set(binance_prices.keys()) & set(kucoin_prices.keys())
+
+    data = []
+
+    for symbol in common_symbols:
+
+        binance_price = binance_prices[symbol]
+
+        kucoin_price = kucoin_prices[symbol]
+
+        arbitrage = round((kucoin_price - binance_price) / binance_price * 100, 2)
+
+        data.append({
+
+            'symbol': symbol,
+
+            'binance_price': binance_price,
+
+            'kucoin_price': kucoin_price,
+
+            'arbitrage': arbitrage
+
+        })
+
+    return render_template('index.html', data=data)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    app.run()
+
